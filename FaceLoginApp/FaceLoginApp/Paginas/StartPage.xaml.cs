95e9b86a527f2352b693df4cfa4e26872a2dcb5b
Oscar Correa Tarazona <42037739+oscferct12@users.noreply.github.com>
2018-10-28 22:22:32 -0500

    Se cambian labels y nombres en los cammpos

--- FaceLoginApp/FaceLoginApp/Paginas/StartPage.xaml.cs
+++ FaceLoginApp/FaceLoginApp/Paginas/StartPage.xaml.cs
@@ -23,7 +23,7 @@ namespace FaceLoginApp.Paginas
             Loading(true);
             await Task.Delay(5000);
             Loading(false);
-            await Navigation.PushAsync(new PaginaCrearEmpresa());
+            await Navigation.PushAsync(new TabPageRegister());
         }
 
         void Loading(bool mostrar)
